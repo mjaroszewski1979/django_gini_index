9eb07747659d68604dcdce04cdf46bbd5e658797
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-04-26 21:12:46 +0200

    added figure styling

--- htmx/views.py
+++ htmx/views.py
@@ -44,7 +44,7 @@ def index(request):
         countries.append(x[0])
         vals.append(x[1])
     cds = ColumnDataSource(data=dict(countries=countries, vals=vals))
-    fig = figure(x_range=countries, height=500, title=f"GINI Index for ({year})")
+    fig = figure(x_range=countries, sizing_mode='stretch_both', title=f"GINI Index for ({year})")
     fig.title.align = 'center'
     fig.title.text_font_size = '1.5em'
     fig.xaxis.major_label_orientation = math.pi / 4
@@ -63,10 +63,13 @@ def index(request):
     if request.htmx:
         return render(request, 'partials/chart.html', context)
 
-    return render(request, 'index.html', context)
+    return render(request, 'generic.html', context)
 
 def home(request):
     return render(request, 'home.html')
+
+'''def generic(request):
+    return render(request, 'generic.html')'''
 
 
 
