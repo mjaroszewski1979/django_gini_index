0ba9ac303588344d12b3a0259df5ddef0f35c769
Maciej Jaroszewski <mjaroszewski1979@gmail.com>
2022-05-01 09:57:33 +0200

    added class in utls file

--- htmx/utilities.py
+++ htmx/utilities.py
@@ -1,6 +1,10 @@
 import pandas_datareader as pdr
 from pandas_datareader._utils import RemoteDataError
 import pandas as pd
+from bokeh.models import ColumnDataSource, HoverTool
+from bokeh.embed import components
+from bokeh.plotting import figure
+import math
 
 
 
@@ -29,15 +33,50 @@ class GiniIndex:
     def get_data(self):
         result = None
         for symbol in self.symbols:
-            df = pdr.DataReader(symbol['symbol'], 'fred', start=self.start, end=self.end)
-            df = df.rename(columns={df.columns[0]: symbol['title']})
-            if result is None:
-                result = df.copy()
-            else:
-                result = pd.merge_asof(left=result.copy(), right=df, on='DATE')
-            
-        result = result.set_index('DATE')
+            try:
+                df = pdr.DataReader(symbol['symbol'], 'fred', start=self.start, end=self.end)
+                df = df.rename(columns={df.columns[0]: symbol['title']})
+                if result is None:
+                    result = df.copy()
+                else:
+                    result = pd.merge_asof(left=result.copy(), right=df, on='DATE')
+                    result = result.set_index('DATE')
+            except RemoteDataError:
+                result = {'Error': ['RemoteDataError']}
         return result
+
+    def get_inputs(self):
+        fred_data = self.get_data()
+        data = {}
+        for item in fred_data:
+            data[item] = fred_data[item][0]
+        sorted_data = sorted(data.items(), key=lambda x: x[1])
+        for obj in sorted_data:
+            self.countries.append(obj[0])
+            self.vals.append(obj[1])
+
+    def get_context(self, year):
+        self.get_inputs()
+        cds = ColumnDataSource(data=dict(countries=self.countries, vals=self.vals))
+        fig = figure(x_range=self.countries, sizing_mode='stretch_both', title=f"GINI Index for ({year})")
+        fig.title.align = 'center'
+        fig.title.text_font_size = '1.5em'
+        fig.xaxis.major_label_orientation = math.pi / 4
+        fig.vbar(source=cds, x='countries', top='vals', width=0.8 )
+        tooltips = [
+            ('Country', '@countries'),
+            ('GINI', '@vals')
+        ]
+        fig.add_tools(HoverTool(tooltips=tooltips))
+        script, div = components(fig)
+        context = {
+            'script': script,
+            'div': div,
+            'years': range(2010,2019)
+        }
+        return context
+
+
     
 
 
--- htmx/views.py
+++ htmx/views.py
@@ -1,45 +1,12 @@
-import math
 from django.shortcuts import render
-from bokeh.models import ColumnDataSource, HoverTool
-from bokeh.embed import components
-from bokeh.plotting import figure
 from .utilities import GiniIndex
-
-years = range(2010,2019)
-default_year = 2008
 
 
 def index(request):
     
-    year = request.GET.get('year', default_year)
+    year = request.GET.get('year', 2008)
     gi = GiniIndex(start=year, end=year)
-    fred_data = gi.get_data()
-    data = {}
-    for x in fred_data:
-        data[x] = fred_data[x][0]
-    a = sorted(data.items(), key=lambda x: x[1])
-    countries = []
-    vals = []
-    for x in a:
-        countries.append(x[0])
-        vals.append(x[1])
-    cds = ColumnDataSource(data=dict(countries=countries, vals=vals))
-    fig = figure(x_range=countries, sizing_mode='stretch_both', title=f"GINI Index for ({year})")
-    fig.title.align = 'center'
-    fig.title.text_font_size = '1.5em'
-    fig.xaxis.major_label_orientation = math.pi / 4
-    fig.vbar(source=cds, x='countries', top='vals', width=0.8 )
-    tooltips = [
-        ('Country', '@countries'),
-        ('GINI', '@vals')
-    ]
-    fig.add_tools(HoverTool(tooltips=tooltips))
-    script, div = components(fig)
-    context = {
-        'script': script,
-        'div': div,
-        'years': years
-    }
+    context = gi.get_context(year=year)
     if request.htmx:
         return render(request, 'partials/chart.html', context)
 
